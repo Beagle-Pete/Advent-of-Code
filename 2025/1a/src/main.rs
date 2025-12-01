e33c93477d9b8122a01a78b43bdb1c5d6d05ad45
Peter Pena <serv.github@peterpena.com>
2025-12-01 17:46:39 -0500

    Move logic that calculates the number of times  dial stops at 0 into its own function. Rename variables to make it easier to read

--- 2025/1a/src/main.rs
+++ 2025/1a/src/main.rs
@@ -8,49 +8,17 @@ fn main() {
     // let input_file_path = "data/test_input.txt".to_owned();
     let input_file_path = "data/puzzle_input.txt".to_owned();
 
-    let direction = parse_input(input_file_path).unwrap();
-    let mut final_val: Vec<i32> = vec![];
-    let mut zero_count = 0;
+    let dial_clicks = parse_input(input_file_path).expect("Failed to parse input file");
 
-    let mut point = start_pos;
-    for x in &direction {
+    let zero_count = stops_at_zero_count(&start_pos, &dial_clicks);
 
-        let point_test = point + x;
-        let mut rem = 0;
-
-        if point_test < 0 {
-            
-            if point == 0 {
-                rem = *x%100;
-            } else {
-                rem = (x+point)%100;
-            }
-
-            point = 100 - rem.abs();
-        } else if point_test > 100 {
-            rem = point_test%100;
-            point = rem.abs();
-        } else {
-            point = point + x%100;
-        }
-        
-        if point == 100 {
-            point = 0;
-        }
-
-        if point == 0 {
-            zero_count += 1;
-        }
-
-        // Panic if a value is <0 or >100
-        if point < 0 || point > 99 {
-            panic!("Point: {} at direction {}, point_test: {}", point, x, point_test);
-        }
-
-        final_val.push(point);
-    }
-
+    println!();
+    println!("Part 1: ");
     println!("Zero Count: {}", zero_count);
+
+    println!();
+    println!("Part 2:");
+    println!("Zero Count:");
 }
 
 fn parse_input(file_path: String) -> Result<Vec<i32>, Box<dyn error::Error>> {
@@ -79,12 +47,55 @@ fn parse_input(file_path: String) -> Result<Vec<i32>, Box<dyn error::Error>> {
         let rest = rest.parse::<i32>()?;
 
         match first_char {
-            'R' => direction.push(rest),
             'L' => direction.push(-1*rest),
+            'R' => direction.push(rest),
             _ => panic!("Unrecognized prefix: {}", first_char),
         }
         
     }
     
     Ok(direction)
+}
+
+fn stops_at_zero_count(start_pos: &i32, dial_clicks: &Vec<i32>) -> i32 {
+
+    let mut pos = start_pos.clone();
+    let mut zero_count = 0;
+    let mut final_val: Vec<i32> = vec![];
+
+    for dial_click in dial_clicks {
+
+        let new_pos = pos + dial_click;
+        let mut rem = 0;
+
+        if new_pos < 0 {            
+            rem = (dial_click+pos)%100;
+            pos = 100 - rem.abs();
+
+        } else if new_pos > 100 {
+            rem = new_pos%100;
+            pos = rem.abs();
+
+        } else {
+            pos = pos + dial_click%100;
+        }
+        
+        if pos == 100 {
+            pos = 0;
+        }
+
+        if pos == 0 {
+            zero_count += 1;
+        }
+
+        // Panic if a value is <0 or >100
+        if pos < 0 || pos > 99 {
+            panic!("Pos: {} -> Dial Clicks {} -> New Pos: {}", pos, dial_click, new_pos);
+        }
+
+        final_val.push(pos);
+    }
+
+    zero_count
+
 }
